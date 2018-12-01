d5e1fb147c800f6e162364246ab2f384a0f7c0e7
Djeison Hart <djeison@codengage.com>
2018-12-01 17:12:56 -0200

    (task01) Initial Architecture

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  flickr: {
+    KEY: '26bacf1e85a4a41ed916fe4b862a4c05',
+    SECRET: '5657e58e426c7695'
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  flickr: {
+    KEY: '26bacf1e85a4a41ed916fe4b862a4c05',
+    SECRET: '5657e58e426c7695'
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
